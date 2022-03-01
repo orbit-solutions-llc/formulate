d3ec2197ea645052f54cc882a6b8a498362f88d8
Orville Bennett <dev@thebennettproject.com>
2022-03-01 01:57:32 -0500

    Make email sending DRYer.

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,25 @@ struct SubmitAsJson {
     message: String,
 }
 
+fn send_email(
+    form_email: &str,
+    form_full_name: &str,
+    form_subject: &str,
+    form_message: &str,
+) -> Result<(), std::io::Error> {
+    let mail_subject = format!("You have a new inquiry from {}!", DEST_DOMAIN);
+
+    email::send(
+        form_email,
+        [RETURN_EMAIL],
+        &mail_subject,
+        &format!(
+            "From {},\nSubject: {}\n{}",
+            form_full_name, form_subject, form_message
+        ),
+    )
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -41,25 +60,29 @@ fn index() -> &'static str {
 #[post("/", data = "<form>")]
 fn submit(form: Form<Submission<'_>>) -> Result<(), std::io::Error> {
     println!("{:?}", form);
-    let result = email::send(
-        form.email,
-        [RETURN_EMAIL],
-        form.subject,
-        &format!("From {},\n{}", form.full_name, form.message),
-    );
-    Ok(result?)
+
+    let result = send_email(form.email, form.full_name, form.subject, form.message);
+    match result {
+        Ok(_) => {
+            println!("Thank you!  We'll get in touch as soon as we have a response.");
+            result
+        }
+        Err(error) => Err(error),
+    }
 }
 
 #[post("/", format = "json", data = "<form>", rank = 2)]
 fn submit_json(form: Json<SubmitAsJson>) -> Result<(), std::io::Error> {
     println!("{:?}", form);
-    let result = email::send(
-        &form.email,
-        [RETURN_EMAIL],
-        &form.subject,
-        &format!("From {},\n{}", form.full_name, form.message),
-    );
-    Ok(result?)
+
+    let result = send_email(&form.email, &form.full_name, &form.subject, &form.message);
+    match result {
+        Ok(_) => {
+            println!("Thank you! We'll get in touch as soon as we have a response.");
+            result
+        }
+        Err(error) => Err(error),
+    }
 }
 
 #[launch]
