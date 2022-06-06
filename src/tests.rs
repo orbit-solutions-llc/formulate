a2c52b56df7c7708d226f18e183c7f80be294f99
Orville Bennett <dev@thebennettproject.com>
2022-06-06 19:15:28 -0400

    Formatting changes.

--- src/tests.rs
+++ src/tests.rs
@@ -4,7 +4,11 @@ use super::strings::WELCOME_MSG;
 use rocket::http::{ContentType, Status};
 use rocket::local::blocking::{Client, LocalResponse};
 
-fn create_response<'c, 'r>(client: &'c Client, response_type: ContentType, response_body: &'r str) -> LocalResponse<'c> {
+fn create_response<'c, 'r>(
+    client: &'c Client,
+    response_type: ContentType,
+    response_body: &'r str,
+) -> LocalResponse<'c> {
     client
         .post(rocket::uri!(super::submit))
         .header(response_type)
@@ -15,48 +19,46 @@ fn create_response<'c, 'r>(client: &'c Client, response_type: ContentType, respo
 #[test]
 fn test_index() {
     match Client::tracked(rocket()) {
-      Ok(client) => {
-        let response = client.get(rocket::uri!(super::index)).dispatch();
-        assert!(response.status() == Status::Ok);
-        assert!(response.into_string() == Some(WELCOME_MSG.into()));
-      }
-      Err(error) => panic!("Invalid rocket instance: {error}")
+        Ok(client) => {
+            let response = client.get(rocket::uri!(super::index)).dispatch();
+            assert!(response.status() == Status::Ok);
+            assert!(response.into_string() == Some(WELCOME_MSG.into()));
+        }
+        Err(error) => panic!("Invalid rocket instance: {error}"),
     }
 }
 
 #[test]
 fn test_submit() {
     match Client::tracked(rocket()) {
-      Ok(client) => {
-        let form_body = "fullname=Full+Name&email=test%40test.com&subject=mail+title&message=You+have+a+new+inquiry+from&site=site.com";
-        let response = create_response(&client, ContentType::Form, form_body);
-        assert!(response.status() == Status::Ok);
-        assert!(response.into_string() == Some(super::SUCCESS_MSG.into()));
+        Ok(client) => {
+            let lowercase_form_body = "fullname=Full+Name&email=test%40test.com&subject=mail+title&message=You+have+a+new+inquiry+from&site=site.com";
+            let response = create_response(&client, ContentType::Form, lowercase_form_body);
+            assert!(response.status() == Status::Ok);
+            assert!(response.into_string() == Some(super::SUCCESS_MSG.into()));
 
-        let malformed_email_body = "fullname=Full+Name&email=testtest.com&subject=mail+title&message=You+have+a+new+inquiry+from&site=site.com";
-        let response = create_response(&client, ContentType::Form, malformed_email_body);
-        assert!(response.status() == Status::BadRequest);
-      }
-      Err(error) => panic!("Invalid rocket instance: {error}")
+            let malformed_email_body = "fullname=Full+Name&email=testtest.com&subject=mail+title&message=You+have+a+new+inquiry+from&site=site.com";
+            let response = create_response(&client, ContentType::Form, malformed_email_body);
+            assert!(response.status() == Status::BadRequest);
+        }
+        Err(error) => panic!("Invalid rocket instance: {error}"),
     }
-
 }
 
 #[test]
 fn test_submit_json() {
     match Client::tracked(rocket()) {
-      Ok(client) => {
-        let form_body = "{\"fullname\":\"Named\",\"email\":\"publico@thebennettproject.com\",\"subject\":\"mail\",\"message\":\"You have a new inquiry from\",\"site\":\"site.com\"}";
-        let response = create_response(&client, ContentType::JSON, form_body);
-        assert!(response.status() == Status::Ok);
-        assert!(response.into_string() == Some(super::SUCCESS_MSG.into()));
-    
-        let malformed_email_body = "{\"fullname\":\"Named\",\"email\":\"testtest.com\",\"subject\":\"mail\",\"message\":\"You have a new inquiry from\",\"site\":\"site.com\"}";
-        let response = create_response(&client, ContentType::JSON, malformed_email_body);
-        assert!(response.status() == Status::BadRequest);
-            assert!(response.into_string().unwrap() == String::from("Missing domain or user"));
-      }
-      Err(error) => panic!("Invalid rocket instance: {error}")
-    }
+        Ok(client) => {
+            let form_body = "{\"fullname\":\"Named\",\"email\":\"publico@thebennettproject.com\",\"subject\":\"mail\",\"message\":\"You have a new inquiry from\",\"site\":\"site.com\"}";
+            let response = create_response(&client, ContentType::JSON, form_body);
+            assert!(response.status() == Status::Ok);
+            assert!(response.into_string() == Some(super::SUCCESS_MSG.into()));
 
+            let malformed_email_body = "{\"fullname\":\"Named\",\"email\":\"testtest.com\",\"subject\":\"mail\",\"message\":\"You have a new inquiry from\",\"site\":\"site.com\"}";
+            let response = create_response(&client, ContentType::JSON, malformed_email_body);
+            assert!(response.status() == Status::BadRequest);
+            assert!(response.into_string().unwrap() == String::from("Missing domain or user"));
+        }
+        Err(error) => panic!("Invalid rocket instance: {error}"),
+    }
 }
