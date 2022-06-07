a46558defab27e4adf5d02df2c23584331f2366b
Orville Bennett <dev@thebennettproject.com>
2022-06-07 01:52:44 -0400

    Add email validation msg.

--- src/strings.rs
+++ src/strings.rs
@@ -1,2 +1,4 @@
 pub const SUCCESS_MSG: &str = "Thank you! We'll get in touch as soon as we're able to.";
 pub const WELCOME_MSG: &str = "for·mu·late, verb, to create or devise methodically.";
+pub const EMAIL_VALIDATION_MSG: &str =
+    "Invalid email address provided. Please check email and try again.";
